a195f07666925a9763c2000ba94e4caa9bc1392f
unknown <shihsh@ceiec.com.cn>
2022-03-24 18:07:33 +0800

    20220324

--- leetcode/programming_questions_and_solutions/closed_number_05_04.py
+++ leetcode/programming_questions_and_solutions/closed_number_05_04.py
@@ -3,3 +3,46 @@
 # @Time    : 2022/3/21 16:49
 # @Author  : Marc
 # @File    : closed_number_05_04.py
+
+class ClosedNumber:
+    def findClosedNumbers(self, num: int) -> list:
+        """
+        较小数：找到 尽可能低位的 模式串'10' 翻转；若全为1则不存在更小数
+        较大数：找到 尽可能低位的 模式串'01' 翻转，若全为1则加一位1，第二位1变0
+        """
+        b = str(bin(num))[2:]
+        if not '0' in b:
+            if int('0b10' + b[1:], 2) < int('08fffffff', 16):
+                return [int('0b10'+b[1:], 2), -1]
+            else:
+                return[-1, -1]
+        b = '0' + b
+        smaller, bigger = None, None
+        for i in range(len(b)-1, -1, -1):
+            if b[i:i+2] == '10':
+                tmp = b[i+2:]
+                tmp = '1' * tmp.count('1') + '0' * tmp.count('0')
+                smaller = b[:i] + '01' + tmp
+                break
+        for i in range(len(b)-1, -1, -1):
+            if b[i:i+2] == '01':
+                tmp = b[i+2:]
+                tmp = '0' * tmp.count('0') + '1' * tmp.count('1')
+                bigger = b[:i] + '10' + tmp
+                break
+        print("****************************")
+        print(int(bigger,2))
+        print(int('08fffffff', 16))
+        if int(bigger, 2) < int('08fffffff', 16):
+            return [int(bigger, 2), int(smaller, 2)]
+        return [-1, int(smaller, 2)]
+
+
+def main():
+    num = 2147483647
+    test = ClosedNumber()
+    res = test.findClosedNumbers(num)
+    print(res)
+
+if __name__ == "__main__":
+    main()
--- leetcode/programming_questions_and_solutions/reverse_bits_05_03.py
+++ leetcode/programming_questions_and_solutions/reverse_bits_05_03.py
@@ -3,3 +3,20 @@
 # @Time    : 2022/3/21 16:39
 # @Author  : Marc
 # @File    : reverse_bits_05_03.py
+
+class ReverseBits:
+    def reverseBits(self, num: int) -> int:
+        """
+        :type num: int
+        :rtype: int
+        """
+        current_one, insert_one, ans = 0, 0, 0
+        for i in range(32):
+            if num & (1<<i):
+                current_one += 1
+                insert_one += 1
+            else:
+                insert_one = current_one + 1
+                current_one = 0
+            ans = max(ans, insert_one)
+        return ans
